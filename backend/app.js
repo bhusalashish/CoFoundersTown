b5da87efca15ae68a224e250591926a67725f202
bhusalashish <ashish.bhusal.16999@gmail.com>
2021-01-06 00:12:28 +0530

    setup mongodb atlas

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
 
-mongoose.connect("url(mongodb://127.0.0.1:27017/todos)", {
+const uri = process.env.MONGODB_URI;
+console.log(uri);
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
 const connection = mongoose.connection;
 connection.once("open", function () {
     console.log("MongoDB database connection established successfully");
 });
 
-const PORT = 4000;
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
